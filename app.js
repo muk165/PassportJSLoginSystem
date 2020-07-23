b6fda980ef4f1e7e550330f57c4fe1d706352d91
muk165 <mukteshg156@gmail.com>
2020-07-23 20:43:12 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+const app = express();
+
+
